84a02b5dc337faf47f538deb29c673903c54e3d0
Karan Suthar <168117063+KaranJangid8656@users.noreply.github.com>
2025-01-19 19:26:47 +0530

    Update s2n-config.cmake

--- cmake/s2n-config.cmake
+++ cmake/s2n-config.cmake
@@ -10,6 +10,8 @@ find_dependency(crypto)
 
 # Allow static or shared lib to be used.
 # If both are installed, choose based on BUILD_SHARED_LIBS.
+
+
 if (BUILD_SHARED_LIBS)
     if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/shared")
         include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
